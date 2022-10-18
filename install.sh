5e8d177132531707d29341795d99031a29872013
SirLion02 <109844462+SirLion02@users.noreply.github.com>
2022-10-18 18:37:29 +0700

    edit dikit

--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
-# install xorg + display driver
-sudo pacman -S xorg xorg-xinit xf86-video-amdgpu
-
 # install additional package
-sudo pacman -S i3-gaps thunar htop neofetch git neovim light picom lxappearance alacritty polybar nitrogen flameshot rofi wget unzip links qutebrowser
+sudo pacman -S i3-gaps pcmanfm bashtop neovim picom polkit network-manager-applet lxappearance alacritty polybar nitrogen flameshot rofi wget unzip qutebrowser
 
 #cd dotfiles/
 cp -rv i3/ alacritty/ polybar/ ~/.config/
@@ -13,17 +10,17 @@ chmod +x ~/.config/i3/volume-max-100.sh
 cp /etc/X11/xinit/xinitrc ~/.xinitrc
 
 # Font for polybar
-wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/FiraCode.zip
-wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Hack.zip
-
 mkdir Nerd\ Fonts
+
 unzip FiraCode.zip -d Nerd\ Fonts
 unzip Hack.zip -d Nerd\ Fonts
 
 sudo mv Nerd\ Fonts /usr/share/fonts
 
-rm FiraCode.zip
-rm Hack.zip
+#font for alacritty
+unzip ubuntu-font-family-0.83.zip
+
+sudo mv ubuntu-font-family-0.83/
 
 # wallpaper
-sudo cp wallpaper.jpeg /usr/share/backgrounds/wallpaper.jpeg
+sudo cp -rv low-poly* wallpaper.jpeg /usr/share/backgrounds/
